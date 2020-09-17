98f00287f665bc4195859cb1f1394aae26948c17
emalvido <emalvido2@hotmail.com>
2020-09-17 20:19:46 +0200

    initial collection view setup

---
+++ View/ProductCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Eduardo Malvido on 9/17/20.
+//  Copyright © 2020 Eduardo Malvido. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImg: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+    
+}
