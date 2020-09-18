ceec418c08a8fc718d02a01265ef66ac409341c8
emalvido <emalvido2@hotmail.com>
2020-09-18 14:36:50 +0200

    starting product screens

---
+++ View/ProductCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Eduardo Malvido on 9/18/20.
+//  Copyright © 2020 Eduardo Malvido. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    @IBOutlet weak var productImg: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+
+//    func uptdateViews(product: Product) {
+//        productImg.image = UIImage(named: product.imageName)
+//        productTitle.text = product.title
+//        productPrice.text = product.price
+//    }
+
+}
